7e426cd891bb36b4bead5b851ef032ff9899bb73
mechztcc <dojathan@gmail.com>
2024-02-08 07:55:31 -0500

    feat: login form

--- components/default-button.vue
+++ components/default-button.vue
@@ -1,9 +1,6 @@
 <template>
   <NuxtLink :to="to" @click="pressed()">
-    <button
-      type="button"
-      :class="['p-2 hover:bg-zinc-900 hover:text-green-400 rounded trans', fill ? 'bg-zinc-900 w-full text-white': '']"
-    >
+    <button :type="type =='button' ? 'button': 'submit'" :class="['p-2 hover:bg-zinc-900 hover:text-green-400 rounded trans', fill ? 'bg-zinc-900 w-full text-white' : '']">
       <slot></slot>
       <span class="text-lg"> {{ label }}</span>
     </button>
@@ -14,13 +11,16 @@
 const props = defineProps({
   label: { type: String },
   fill: { type: Boolean, default: false },
-  to: { type: String, default: "", required: false },
+  to: { type: String, default: '', required: false },
+  type: {
+    type: String,
+  },
 });
 
-const emit = defineEmits(["pressed"]);
+const emit = defineEmits(['pressed']);
 
 const pressed = () => {
-  emit("pressed", true);
+  emit('pressed', true);
 };
 </script>
 
--- components/login/form.vue
+++ components/login/form.vue
@@ -1,5 +1,5 @@
 <template>
-  <Form class="flex flex-col justify-between h-full w-full">
+  <Form class="flex flex-col justify-between h-full w-full" :validation-schema="schema" @submit="onSubmit">
     <div class="flex justify-center mt-5"></div>
     <div class="flex flex-col">
       <div class="flex justify-center mb-2">
@@ -21,7 +21,7 @@
       </DefaultInput>
 
       <span class="text-zinc-700 text-end mt-2 cursor-pointer"> Esqueceu a senha? </span>
-      <DefaultButton :label="'ENTRAR'" :fill="true" class="mt-5" :to="'/'" />
+      <DefaultButton :label="'ENTRAR'" :fill="true" class="mt-5" :type="'submit'" />
       <ButtonGoogle />
     </div>
     <NuxtLink :to="'/create-account'" class="text-zinc-700 mb-5 text-center">
@@ -34,12 +34,41 @@
 </template>
 
 <script setup lang="ts">
-import { Form, Field, ErrorMessage } from 'vee-validate';
+import { Form } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as zod from 'zod';
+
+const schema = toTypedSchema(
+  zod.object({
+    email: zod.string().email({ message: 'E-mail inválido' }),
+    password: zod.string().min(6, { message: 'Senha é obrigatório' }),
+  })
+);
+
+const payload = ref({
+  email: '',
+  password: '',
+});
+
+const body = computed(() => {
+  return payload.value;
+});
+
+const { data, pending, execute } = useFetch('users', {
+  method: 'POST',
+  immediate: false,
+  body,
+});
+
 
 const isPass = ref(true);
-
 function onHandlePass() {
   isPass.value = !isPass.value;
+}
+
+async function onSubmit(v: any) {
+  payload.value = v;
+  await execute();
 }
 </script>
 
