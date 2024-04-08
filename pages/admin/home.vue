29dc73c90e5ccf467365dbfbeb1b80c813f56be8
mechztcc <dojathan@gmail.com>
2024-04-08 10:34:15 -0300

    feat: add search offers page

--- pages/admin/home.vue
+++ pages/admin/home.vue
@@ -1,9 +1,5 @@
 <template>
   <NuxtLayout :name="'admin'">
-    <span class="text-xl font-bold">Aguardando seu lance</span>
-    <div class="grid grid-cols-1 md:grid-cols-3 mb-10">
-      <DashboardCardAvailableOffer v-for="(item, index) in 5" :key="index" />
-    </div>
     <DashboardTableBinds />
   </NuxtLayout>
 </template>
---
+++ pages/admin/search-offers.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout :name="'admin'">
+    <span class="text-xl font-bold">Aguardando seu lance</span>
+    <div class="grid grid-cols-1 md:grid-cols-3 mb-10">
+      <DashboardCardAvailableOffer v-for="(item, index) in 5" :key="index" />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
