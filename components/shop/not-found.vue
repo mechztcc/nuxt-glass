8438a47824e7385b6e525094b5e1721d0c2219fd
mechztcc <dojathan@gmail.com>
2024-04-08 21:46:10 -0300

    feat: add component to validate if user has a store created

---
+++ components/shop/not-found.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid grid-cols-1" v-if="data.length == 0">
+    <div class="col-span-1">
+      <div class="flex flex-col justify-center items-center min-h-screen">
+        <font-awesome-icon :icon="['fas', 'store-slash']" :size="'2xl'" />
+        <span class="text-xl">Você ainda não possui uma loja registrada</span>
+        <DefaultButton class="mt-2" :label="'Quero criar uma loja'" :fill="true" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { data, pending } = useFetchAuth('store', { immediate: true, method: 'get' });
+</script>
+
+<style scoped></style>
--- pages/admin/shop.vue
+++ pages/admin/shop.vue
@@ -1,17 +1,10 @@
 <template>
   <NuxtLayout :name="'admin'">
-    <div class="grid grid-cols-1">
-      <div class="col-span-1">
-        <div class="flex flex-col justify-center items-center min-h-screen">
-          <font-awesome-icon :icon="['fas', 'store-slash']" :size="'2xl'" />
-          <span class="text-xl">Você ainda não possui uma loja registrada</span>
-          <DefaultButton class="mt-2" :label="'Quero criar uma loja'" :fill="true" />
-        </div>
-      </div>
-    </div>
+    <ShopNotFound />
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <style scoped></style>
